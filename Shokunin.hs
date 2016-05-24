7df421c95753697c2a06c606a01a955f8a28f619
Charlie Brooking <cbrookin@thoughtworks.com>
2016-05-25 01:07:24 +1000

    add wonky renderer

--- Shokunin.hs
+++ Shokunin.hs
@@ -5,20 +5,16 @@ import Data.Maybe
 
 parse line = parseTimeM False defaultTimeLocale "%H:%M" line :: Maybe TimeOfDay
 validate time = time >>= \t -> if todHour t < 24 && todMin t < 60 then Just t else Nothing
+hourSymbol time hour = if (todHour time) == hour then 'h' else 'o'
+x h = round(8 * sin((2 * pi / 12) * h) + 8)
+y h = round(-5 * cos((2 * pi / 12) * h) + 5)
+hourPositions = [(x(h), y(h)) | h <- [0..23]]
 
-clockLines time = [
-    "        h        ",
-    "    o       o    ",
-    "                 ",
-    " o             o ",
-    "                 ",
-    "o               o",
-    "                 ",
-    " o             o ",
-    "                 ",
-    "    o       o    ",
-    "        o        "
-  ]
+clockLines time = lines
+  where
+    lines = map line [0..10]
+    line y = concat(map (\x -> symbol x y) [0..16])
+    symbol x y = if elem (x, y) hourPositions then "o" else " "
 
 outputLines line = case (validate . parse) line of
   Just time -> clockLines time
