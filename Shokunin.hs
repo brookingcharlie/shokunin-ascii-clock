09966edd7772cb6c33cd9733c31e69a6766644d0
Charlie Brooking <cbrookin@thoughtworks.com>
2016-05-25 09:24:44 +1000

    go around clock once

--- Shokunin.hs
+++ Shokunin.hs
@@ -6,16 +6,17 @@ import Data.Time
 
 parse line = parseTimeM False defaultTimeLocale "%H:%M" line :: Maybe TimeOfDay
 validate time = time >>= \t -> if todHour t < 24 && todMin t < 60 then Just t else Nothing
-hourSymbol time hour = if (todHour time) == hour then 'h' else 'o'
-x h = round(8 * sin((2 * pi / 12) * h) + 8)
-y h = round(-4.644 * cos((2 * pi / 12) * h) + 5)
-clockPositions = [(x(h), y(h)) | h <- [0..23]]
+x i = round(8 * sin((2 * pi / 12) * i) + 8)
+y i = round(-4.644 * cos((2 * pi / 12) * i) + 5)
+clockPositions = [(x(i), y(i)) | i <- [0..11]]
 
 clockLines time = lines
   where
     lines = map line [0..10]
     line y = concat(map (\x -> symbol x y) [0..16])
-    symbol x y = maybe " " (\h -> if h == todHour time then "h" else "o") (elemIndex (x, y) clockPositions)
+    symbol x y = maybe " "
+      (\i -> if i == ((todHour time) `mod` 12) then "h" else "o")
+      (elemIndex (x, y) clockPositions)
 
 outputLines line = case (validate . parse) line of
   Just time -> clockLines time
