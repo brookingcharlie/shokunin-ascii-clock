c10a216cf17e67bc5cc66fa93cc86309b9f977c9
Charlie Brooking <cbrookin@thoughtworks.com>
2016-05-25 13:14:00 +1000

    clean up code

--- Shokunin.hs
+++ Shokunin.hs
@@ -4,26 +4,27 @@ import Data.List
 import Data.Maybe
 import Data.Time
 
-parse line =
-  (parseTimeM False defaultTimeLocale "%H:%M" line :: Maybe TimeOfDay) >>=
+parseValidTime input =
+  (parseTimeM False defaultTimeLocale "%H:%M" input :: Maybe TimeOfDay) >>=
   \t -> if todHour t < 24 && todMin t < 60 then Just t else Nothing
 
-x i = round(8 * sin((2 * pi / 12) * i) + 8)
-y i = round(-4.644 * cos((2 * pi / 12) * i) + 5)
 markCoords = [(x(i), y(i)) | i <- [0..11]]
+  where
+    x i = round(8 * sin((2 * pi / 12) * i) + 8)
+    y i = round(-4.644 * cos((2 * pi / 12) * i) + 5)
 
-markSymbol time i =
+markChar time i =
   case [matchesHour, matchesMin] of
-    [True, True] -> "x"
-    [True, False] -> "h"
-    [False, True] -> "m"
-    otherwise -> "o"
+    [True, True] -> 'x'
+    [True, False] -> 'h'
+    [False, True] -> 'm'
+    otherwise -> 'o'
   where
     matchesHour = i == (todHour time) `mod` 12
     matchesMin = i == ((todMin time) `div` 5) `mod` 12
-coordSymbol time x y = elemIndex (x, y) markCoords >>= (\i -> Just (markSymbol time i))
 
-clockLines time = map clockLine [0..10]
-  where clockLine y = concat(map (\x -> fromMaybe " " (coordSymbol time x y)) [0..16])
+coordChar time x y = maybe ' ' (markChar time) (elemIndex (x, y) markCoords)
 
-outputLines line = maybe ["INVALID INPUT"] clockLines (parse line)
+clockLines time = [[coordChar time x y | x <- [0..16]] | y <- [0..10]]
+
+outputLines input = maybe ["INVALID INPUT"] clockLines (parseValidTime input)
