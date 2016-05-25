36e3611d8df8d698fda5610a8d72bbf7e0561f56
Charlie Brooking <cbrookin@thoughtworks.com>
2016-05-25 10:37:47 +1000

    passing tests

--- Shokunin.hs
+++ Shokunin.hs
@@ -12,13 +12,19 @@ x i = round(8 * sin((2 * pi / 12) * i) + 8)
 y i = round(-4.644 * cos((2 * pi / 12) * i) + 5)
 markCoordinates = [(x(i), y(i)) | i <- [0..11]]
 
-markSymbol time x y =
-  elemIndex (x, y) markCoordinates >>=
-  \i -> Just (if i == (todHour time) `mod` 12 then "x" else "o")
+markSymbol time i =
+  if matchesHour && matchesMin then "x"
+  else if matchesHour then "h"
+  else if matchesMin then "m"
+  else "o"
+  where
+    matchesHour = i == (todHour time) `mod` 12
+    matchesMin = i == ((todMin time) `div` 5) `mod` 12
+coordinateSymbol time x y = elemIndex (x, y) markCoordinates >>= (\i -> Just (markSymbol time i))
 
 clockLines time = lines
   where
     lines = map line [0..10]
-    line y = concat(map (\x -> fromMaybe " " (markSymbol time x y)) [0..16])
+    line y = concat(map (\x -> fromMaybe " " (coordinateSymbol time x y)) [0..16])
 
 outputLines line = maybe ["INVALID INPUT"] clockLines (parse line)
